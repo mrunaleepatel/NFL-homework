f78c3cd4b6382638fd7636d8a02f2a4d01154af6
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-09-07 13:08:45 -0500

    Commit: NFL - HFM

--- nfl.sql
+++ nfl.sql
@@ -43,6 +43,18 @@ LIMIT 100;
 SELECT AVG(salary) AS salary
 FROM players
 WHERE position = 'DE';
+
 -- 12. The names of all the players on the Buffalo Bills
+SELECT players.name, teams.name
+FROM players, teams
+WHERE players.team_id=teams.id AND teams.name LIKE 'Buffalo Bills';
 -- 13. The total salary of all players on the New York Giants
+SELECT SUM(salary) AS salary
+FROM players
+WHERE name = 'New York Giants';
 -- 14. The player with the lowest salary on the Green Bay Packers
+SELECT name, salary
+FROM players
+WHERE name = 'Green Bay Packers'
+ORDER BY salary ASC
+LIMIT 1;
