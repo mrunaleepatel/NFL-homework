dc61a3d3fe04794bb582f3c1802642b86a0f933b
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-09-06 21:34:56 -0500

    Commit: NFL queries 2

--- nfl.sql
+++ nfl.sql
@@ -13,9 +13,21 @@ SELECT COUNT(*) AS total_players
 FROM players;
 
 -- 5. The team names and head coaches of the NFC North and AFC East
+SELECT name, head_coach
+FROM teams
+WHERE conference IN ('NFC', 'AFC');
 -- 6. The 50 players with the highest salaries
+SELECT name, salary
+FROM players
+ORDER BY salary DESC
+LIMIT 50;
 -- 7. The average salary of all NFL players
+SELECT AVG(salary) AS average_salary
+FROM players;
 -- 8. The names and positions of players with a salary above 10_000_000
+SELECT name, position
+FROM players
+WHERE salary > 10000000;
 -- 9. The player with the highest salary in the NFL
 -- 10. The name and position of the first 100 players with the lowest salaries
 -- 11. The average salary for a DE in the nfl
